da506a5575d3b5828ebc7a4b61aa9c02ec58059f
YejeongKang <dpwjd1130@gmail.com>
2024-06-09 02:09:49 +0900

    Add project files.

---
+++ YachtDicePrototype1/YachtDicePrototype1.h
@@ -0,0 +1,32 @@
+﻿
+// YachtDicePrototype1.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CYachtDicePrototype1App:
+// See YachtDicePrototype1.cpp for the implementation of this class
+//
+
+class CYachtDicePrototype1App : public CWinApp
+{
+public:
+	CYachtDicePrototype1App();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CYachtDicePrototype1App theApp;
---
+++ YachtDicePrototype1/YachtDicePrototype1Dlg.h
@@ -0,0 +1,41 @@
+﻿
+// YachtDicePrototype1Dlg.h : header file
+//
+
+#pragma once
+
+
+// CYachtDicePrototype1Dlg dialog
+class CYachtDicePrototype1Dlg : public CDialogEx
+{
+// Construction
+public:
+	CYachtDicePrototype1Dlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_YACHTDICEPROTOTYPE1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CImage main;
+
+	afx_msg void OnBnClickedButtonSingle();
+	CBitmapButton m_single_bmp;
+	CBitmapButton m_dual_bmp;
+	afx_msg void OnClickedButtonDual();
+};
